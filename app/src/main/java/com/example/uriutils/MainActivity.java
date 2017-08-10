19bba2312f492e3e05be8c8bde35621d08c9f283
timshinlee <timshinlee@gmail.com>
2017-08-10 14:51:19 +0800

    修改example

--- app/src/main/java/com/example/uriutils/MainActivity.java
+++ app/src/main/java/com/example/uriutils/MainActivity.java
@@ -1,10 +1,11 @@
 package com.example.uriutils;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -12,7 +13,7 @@ import android.widget.TextView;
 import com.timshinlee.utils.UriUtils;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = "MainActivity";
     private TextView mText;
     private ImageView mImage;
 
@@ -34,20 +35,18 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void openDocument(View view) {
-        startActivityForResult(new Intent(Intent.ACTION_OPEN_DOCUMENT).setType("image/*"), 0);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            startActivityForResult(new Intent(Intent.ACTION_OPEN_DOCUMENT).setType("image/*"), 0);
+        }
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (resultCode == RESULT_OK && data != null) {
-//            mText.setText(mText.getText() + "\n" + data.getData());
+            Log.e(TAG, "onActivityResult: " + data.getData());
             mText.setText(mText.getText() + "\n" + UriUtils.getUriPath(this, data.getData()));
             mImage.setImageBitmap(BitmapFactory.decodeFile(UriUtils.getUriPath(this, data.getData())));
 //            mImage.setImageBitmap(UriUtils.getBitmapFromUri(this,data.getData()));
         }
-    }
-
-    private void getBitmap(String path){
-        final Bitmap bitmap = BitmapFactory.decodeFile(path);
     }
 }
